1e7d5679b648ff85cddc4e84d89fe7e2cee7d87c
Rhee Kang <rkang840@student.fuhsd.org>
2017-07-10 10:09:40 -0700

    fixed username error

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,7 +114,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-
+ALLOWED_HOSTS = ['127.0.0.1', 'rkang840.pythonanywhere.com']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
